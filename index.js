bdf6f913994c06155f292194bdab8bc74031eb9a
Damilare <32745404+veectorh@users.noreply.github.com>
2020-04-28 11:32:59 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
 const convert = (drop) => {
     return (
-      [
-        [3, "Pling"],
-        [5, "Plang"],
-        [7, "Plong"],
-      ].reduce(
-        (accumulator, [num, sound]) =>
-          drop % num === 0 ? accumulator + sound : accumulator,
-        ""
-      ) || drop.toString()
+     //type function here
     );
   };
 
